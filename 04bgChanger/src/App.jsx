102265882295fcc3df7d970e441dcdd5fc56c16c
Deepak Dhaka <deepa.dhaka.7634@gmail.com>
2024-01-03 20:58:39 +0530

    My First commit

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200"
+        style={{backgroundColor: color}}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" 
+            style={{backgroundColor: "Red"}}
+            onClick={() => {setColor("red")}}//this is a funtion call back which is setting the color to red
+             >Red</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" 
+            style={{backgroundColor: "Green"}} 
+            onClick={() => {setColor("green")}}
+            >Green</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" 
+            style={{backgroundColor: "Blue"}} 
+            onClick={() => {setColor("blue")}}
+            >Blue</button>
+            <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg" 
+            style={{backgroundColor: "White"}} 
+            onClick={() => {setColor("white")}}
+            >White</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" 
+            style={{backgroundColor: "Black"}} 
+            onClick={() => {setColor("black")}}
+            >Black</button>
+            <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg" 
+            style={{backgroundColor: "Yellow"}} 
+            onClick={() => {setColor("yellow")}}
+            >Yellow</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
