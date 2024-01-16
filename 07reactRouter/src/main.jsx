30de1405304a7451623e347cc0615df633b6f994
Deepak Dhaka <deepa.dhaka.7634@gmail.com>
2024-01-16 20:43:08 +0530

    router

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,9 +2,59 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './components/About/About.jsx'
+import Home from './components/Home/home'
+import Contact from './components/Contact/Contact.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import User from './components/User/User.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "About",
+//         element: <About />
+//       },
+//       {
+//         path: "Contact",
+//         element: <Contact />
+//       },
+//       {
+//         path: "GitHub",
+//         element: <Github />
+//       },
+//     ]
+//   }
+// ])
+
+//there are two ways to call files in router one is above and other is below
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>} >
+       <Route path='' element={<Home/>} />
+       <Route path='About' element={<About/>} />
+       <Route path='Contact' element={<Contact />} />
+       <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
